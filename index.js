2b15d5b4c96f02e6c0decdfd32bee37adf6ad8cd
nataliyarosenberg <129983835+nataliyarosenberg@users.noreply.github.com>
2025-08-08 17:08:09 +0000

    Deploying to gh-pages from @ nataliyarosenberg/project-group-11@46200aab9c5096a8f0fc0eb5be5b1c562d3e338f 🚀

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-import{a as c}from"./assets/vendor-BkCUij8E.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(e){if(e.ep)return;e.ep=!0;const r=n(e);fetch(e.href,r)}})();const l=["all","sofa","wardrobe","bed","table","chairs","kitchen","children-room","office","entryway","bathroom","outdoor","decor"],f=document.querySelector(".categories-list"),d=document.querySelector(".furniture-list");async function p(){try{const t=(await c("https://furniture-store.b.goit.study/api/categories")).data;t.unshift({_id:"0",name:"Всі товари"}),g(t)}catch(s){console.log(s.message)}}function g(s){const t=s.map((n,o)=>{const e=l[o];return`
-        <li
-  class="category-card"
-  style="
-  background-image: -webkit-image-set(
-    url('./src/images/our-furniture/${e}@1x.png') 1x,
-    url('./src/images/our-furniture/${e}@2x.png') 2x
-  );
-  background-image: image-set(
-    url('./src/images/our-furniture/${e}@1x.png') 1x,
-    url('./src/images/our-furniture/${e}@2x.png') 2x
-  );
-"
->
-  <p class="category-title">${n.name}</p>
-</li>`}).join("");f.insertAdjacentHTML("beforeend",t)}async function m(){try{const t=(await c("https://furniture-store.b.goit.study/api/furnitures?limit=8")).data;y(t)}catch(s){console.log(s.message)}}function y(s){const n=s.furnitures.map(o=>{const{_id:e,name:r,description:i,price:a,images:u}=o;return`
+import{a as f,i as g}from"./assets/vendor-CyHMW-y7.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const m=document.querySelector(".loader-more"),p=document.querySelector(".loader-more-container"),y=document.querySelector(".furniture-list"),h=document.querySelector(".load-more-btn");let c=1,a=null;const u=8;async function v(){try{const t=(await f("https://furniture-store.b.goit.study/api/categories")).data;t.unshift({_id:"0",name:"Всі товари"}),document.querySelectorAll(".category-card").forEach((s,e)=>{const r=t[e];if(!r)return;s.dataset.id=r._id;const i=s.querySelector(".category-title");i&&(i.textContent=r.name)})}catch{g.show({message:"Sorry, something went wrong. Please try again!",position:"topRight",messageColor:"#fff",titleColor:"#fff",color:"#ef4040",iconUrl:"../images/our-furniture/bi_x-octagon-2.svg",maxWidth:432})}}async function l(n=1,t){try{S(),d();let o=`https://furniture-store.b.goit.study/api/furnitures?limit=${u}&page=${n}`;t&&t!=="0"&&(o+=`&category=${t}`);const e=(await f(o)).data;w(e),a===null&&(a=Math.ceil(e.totalItems/u)),n>=a?d():q()}catch{g.show({message:"Sorry, something went wrong. Please try again!",position:"topRight",messageColor:"#fff",titleColor:"#fff",color:"#ef4040",iconUrl:"../images/our-furniture/bi_x-octagon-2.svg",maxWidth:432})}finally{P()}}function w(n){const t=n.furnitures;window.allFurnitures=t;const o=t.map(s=>{const{_id:e,name:r,description:i,price:L,images:b}=s;return`
     <li class="furniture-card" id="${e}"> 
-  <img src="${u[0]}" alt="${i}" class="furniture-image" />
-  <p class="furniture-name">${r}</p>
-  <div class="color-wrap">
-    <span class="grey-color"></span>
-    <span class="beige-color"></span>
-    <span class="black-color"></span>
-  </div>
-  <p class="furniture-price">${a} грн</p>
+  <img src="${b[0]}" alt="${i}" class="furniture-image" />
+      <p class="furniture-name">${r}</p>
+      <div class="color-wrap">
+        <span class="grey-color"></span>
+        <span class="beige-color"></span>
+        <span class="black-color"></span>
+      </div>
+      <p class="furniture-price">${L} грн</p>
   <button class="details-btn" type="button">Детальніше</button>
-</li>`}).join("");d.insertAdjacentHTML("beforeend",n)}p();m();
+</li>`}).join("");y.insertAdjacentHTML("beforeend",o)}async function C(n){const t=n.target.closest(".category-card");if(!t)return;document.querySelectorAll(".category-card.active").forEach(s=>s.classList.remove("active")),t.classList.add("active");const o=t.dataset.id;O(),c=1,o==="0"?(a=null,l(c)):(a=null,l(c,o))}function M(){c+=1,l(c)}function S(){m.classList.remove("hidden"),p.style.display="flex"}function P(){m.classList.add("hidden"),p.style.display="none"}function q(){h.classList.remove("hidden")}function d(){h.classList.add("hidden")}function O(){y.innerHTML=""}const $=document.querySelector(".categories-list"),x=document.querySelector(".load-more-btn");x.addEventListener("click",M);$.addEventListener("click",C);v();l(c);
 //# sourceMappingURL=index.js.map
