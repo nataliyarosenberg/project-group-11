7ceef7d3134cdfc002e102857e840ef630e66e79
zmigd <zmigdalovich@gmail.com>
2025-08-08 13:55:03 +0300

    add svg, add scripts

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,7 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-          const accordion = new Accordion(".accordion", {
-            duration: 300,
-            showMultiple: false, 
-          });
-          accordion.open(0);
-        });
+import Accordion from "accordion-js";
+import "accordion-js/dist/accordion.min.css";
+export function initAccordion() {
+  document.addEventListener("DOMContentLoaded", () => {
+    const accordion = new Accordion(".accordion", {
+      duration: 300,
+      showMultiple: false,
+    });
+    accordion.open(0);
+  });
+}
